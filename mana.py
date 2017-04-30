9f7247bd6992c79c90ef7efbb61bf8b78a6a5ab5
Ian Daniher <itdaniher@gmail.com>
2017-04-30 05:04:13 +0000

    now with some crude redis'ing

--- mana.py
+++ mana.py
@@ -11,29 +11,26 @@ except:
 
 import time
 
-f = open("/root/"+str(int(time.time()))+".ih.log", 'w')
+import asyncio_redis
 
-def log(metainfo, infohash):
-    if metainfo not in [False, None]:
-        try:
-            out = infohash+' '+ metainfo[b'name'].decode('utf-8')+'\n'
-            sys.stdout.write(out)
-            f.write(out)
-            f.flush()
-        except UnicodeDecodeError:
-            print(infohash+'    (not rendered)')
+f = open("/root/"+str(int(time.time()))+".ih.log", 'w')
 
 class Crawler(Maga):
     def __init__(self, loop=None, active_tcp_limit = 1000, max_per_session = 2500):
         super().__init__(loop)
-        self.seen = []
         self.seen_ct = 0
         self.active = 0
         self.threshold = active_tcp_limit
         self.max = max_per_session
+        asyncio.get_event_loop().run_until_complete(self.connect_redis())
+
+    async def connect_redis(self):
+        self.connection = await asyncio_redis.Connection.create('localhost', 6379)
+
     async def handler(self, infohash, addr, peer_addr = None):
-        if self.running and (self.active < self.threshold) and (self.seen_ct < self.max) and (infohash not in self.seen):
-            self.seen.append(infohash)
+        exists = await self.connection.hexists('hashes', infohash)
+        if self.running and (self.active < self.threshold) and (self.seen_ct < self.max) and not exists:
+            await self.connection.hset('hashes', infohash, '')
             self.seen_ct += 1
             if peer_addr is None:
                 peer_addr = addr
@@ -42,9 +39,21 @@ class Crawler(Maga):
                 infohash, peer_addr[0], peer_addr[1], loop=self.loop
             )
             self.active -= 1
-            log(metainfo, infohash)
-        if (self.seen_ct >= self.max) and (self.active < 100):
+            await self.log(metainfo, infohash)
+        if (self.seen_ct >= self.max):
             self.stop()
+
+    async def log(self, metainfo, infohash):
+        if metainfo not in [False, None]:
+            try:
+                out = infohash+' '+ metainfo[b'name'].decode('utf-8')+'\n'
+                sys.stdout.write(out)
+                f.write(out)
+                f.flush()
+            except UnicodeDecodeError:
+                print(infohash+'    (not rendered)')
+
+
     async def handle_announce_peer(self, infohash, addr, peer_addr):
         await self.handler(infohash, addr, peer_addr)
 
